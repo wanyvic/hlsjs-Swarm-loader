9377e5f98e522cd51b276b947a39b98b53457454
wanyvic <q873040807@gmail.com>
2019-10-23 14:35:23 +0800

    change to eth swarm

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 'use strict'
 
-class HlsjsIPFSLoader {
+class HlsjsSwarmLoader {
   constructor(config) {
-    this.ipfs = config.ipfs
-    this.hash = config.ipfsHash
+    this.bzz = config.bzz
+    this.hash = config.bzzHash
     if (config.debug === false) {
-      this.debug = function() {}
+      this.debug = function () { }
     } else if (config.debug === true) {
       this.debug = console.log
     } else {
       this.debug = config.debug
     }
-    if(config.m3u8provider) {
+    if (config.m3u8provider) {
       this.m3u8provider = config.m3u8provider;
     } else {
       this.m3u8provider = null;
     }
-    if(config.tsListProvider) {
+    if (config.tsListProvider) {
       this.tsListProvider = config.tsListProvider;
     } else {
       this.tsListProvider = null;
@@ -38,7 +38,7 @@ class HlsjsIPFSLoader {
     this.loadInternal()
   }
   /**
-   * Call this by getting the HLSIPFSLoader instance from hls.js hls.coreComponents[0].loaders.manifest.setM3U8Provider()
+   * Call this by getting the HLSSwarmLoader instance from hls.js hls.coreComponents[0].loaders.manifest.setM3U8Provider()
    * @param {function} provider 
    */
   setM3U8Provider(provider) {
@@ -61,10 +61,10 @@ class HlsjsIPFSLoader {
     const urlParts = context.url.split("/")
     const filename = urlParts[urlParts.length - 1]
 
-    if(filename.split(".")[1] === "m3u8" && this.m3u8provider !== null) {
+    if (filename.split(".")[1] === "m3u8" && this.m3u8provider !== null) {
       const res = this.m3u8provider();
       let data;
-      if(Buffer.isBuffer(res)) {
+      if (Buffer.isBuffer(res)) {
         data = buf2str(res)
       } else {
         data = res;
@@ -73,13 +73,13 @@ class HlsjsIPFSLoader {
       callbacks.onSuccess(response, stats, context)
       return;
     }
-    if(filename.split(".")[1] === "m3u8" && this.tsListProvider !== null) {
+    if (filename.split(".")[1] === "m3u8" && this.tsListProvider !== null) {
       var tslist = this.tsListProvider();
       var hash = tslist[filename];
-      if(hash) {
+      if (hash) {
         this.cat(hash).then(res => {
           let data;
-          if(Buffer.isBuffer(res)) {
+          if (Buffer.isBuffer(res)) {
             data = buf2str(res)
           } else {
             data = res;
@@ -92,7 +92,7 @@ class HlsjsIPFSLoader {
       }
       return;
     }
-    getFile(this.ipfs, this.hash, filename, this.debug).then(res => {
+    getFile(this.bzz, this.hash, filename, this.debug).then(res => {
       const data = (context.responseType === 'arraybuffer') ? res : buf2str(res)
       stats.loaded = stats.total = data.length
       stats.tload = Math.max(stats.tfirst, performance.now())
@@ -102,16 +102,16 @@ class HlsjsIPFSLoader {
   }
 }
 
-function getFile(ipfs, rootHash, filename, debug) {
+function getFile(bzz, rootHash, filename, debug) {
   debug(`Fetching hash for '${rootHash}/${filename}'`)
-  if(filename === null) {
-    return ipfs.cat(rootHash).then( value => {
-      debug(`Received data for file '${rootHash}' size: ${value.length}`)
+  if (filename === null) {
+    return bzz.download(rootHash).then(value => {
+      debug(`Received data for file '${rootHash}' size: ${value.size}`)
       return value
     });
   }
-  return ipfs.ls(rootHash).then(res => {
-    const link = res.find(({ name }) => (name === filename))
+  return bzz.list(rootHash).then(res => {
+    const link = res.entries.find(({ path }) => (path === filename))
 
     if (link === undefined) {
       throw new Error(`File not found: ${rootHash}/${filename}`)
@@ -119,9 +119,11 @@ function getFile(ipfs, rootHash, filename, debug) {
 
     debug(`Requesting '${link.path}'`)
 
-    return ipfs.cat(link.hash).then(value => {
-      debug(`Received data for file '${link.path}' size: ${value.length}`)
-      return value
+    return bzz.download(link.hash, { mode: 'raw' }).then(res => {
+      return res.arrayBuffer().then(value => {
+        debug(`Received data for file '${link.path}' size: ${link.size}`)
+        return value
+      })
     })
   })
 }
@@ -130,4 +132,4 @@ function buf2str(buf) {
   return String.fromCharCode.apply(null, new Uint8Array(buf))
 }
 
-exports = module.exports = HlsjsIPFSLoader
+exports = module.exports = HlsjsSwarmLoader
